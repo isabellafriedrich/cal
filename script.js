d4a913adeea6ec513664b814e067c5a57f972abb
isaッ <105065931+isabellafriedrich@users.noreply.github.com>
2023-10-29 21:49:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 function calcular() {
-    const R = Number(document.querySelector("#raio").value); // Raio
-    const ω = Number(document.querySelector("#velocidadeAngular").value); // Velocidade Angular
-    const T = Number(document.querySelector("#tempo").value); // Tempo
+    // Obtém o valor do raio do elemento de entrada.
+    // A função parseFloat analisa um argumento (convertendo-o para uma string primeiro caso necessário) e retorna um número de ponto flutuante (número decimal).
+    // Document.querySelector: Retorna o primeiro elemento dentro do documento.
+    const r = parseFloat(document.querySelector("input[name='raio']").value);
 
-    const v = ω * R ; // Velocidade
-    const a = ω / T ;  // Aceleração
+    // Obtém o valor da velocidade do elemento de entrada.
+    const v = parseFloat(document.querySelector("input[name='velocidade']").value);
 
-    document.querySelector("#velocidadefinal").textContent = "Velocidade:" + y.toFixed(2);
-    document.querySelector("#aceleração").textContent = "Aceleração: " + x.toFixed(2);
+    // Calcula a aceleração centrípeta usando a fórmula A = r * v^2.
+    const aceleracaoCentripeta = r * Math.pow(v, 2);
+
+    // Exibe o resultado no elemento HTML com o ID "aceleracaoCentripeta".
+    // document.getElementById: Retorna a referência do elemento através do seu ID.
+    // innerText: retorna apenas o texto. Utiliza-se quando queremos buscar apenas o texto visível disponível no elemento.
+    document.getElementById("aceleracaoCentripeta").innerText = "Aceleração Centrípeta: " + aceleracaoCentripeta.toFixed(2) + " m/s²";
 }
-
-document.addEventListener("DOMContentLoaded", () => {
-    const calcularButton = document.querySelector("#Calcule!");
-    calcularButton.addEventListener("click", calcular);
-});
